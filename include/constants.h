df1954fed698e11b0114bf63c08ed439751b45c2
robertazzopardi <robertazzopardi@icloud.com>
2021-03-18 10:03:52 +0000

    Changed to relative paths for the imgs

--- include/constants.h
+++ include/constants.h
@@ -24,11 +24,11 @@
 #define PLAYER_1 '1'
 #define PLAYER_2 '2'
 
-#define PAWN_IMG "/Users/rob/_CODE/C/chess/resources/864630-chess/svg/008-pawn.svg"
-#define ROOK_IMG "/Users/rob/_CODE/C/chess/resources/864630-chess/svg/006-rook.svg"
-#define KNIGHT_IMG "/Users/rob/_CODE/C/chess/resources/864630-chess/svg/010-horse.svg"
-#define BISHOP_IMG "/Users/rob/_CODE/C/chess/resources/864630-chess/svg/016-bishop.svg"
-#define QUEEN_IMG "/Users/rob/_CODE/C/chess/resources/864630-chess/svg/007-queen.svg"
-#define KING_IMG "/Users/rob/_CODE/C/chess/resources/864630-chess/svg/009-king.svg"
+#define PAWN_IMG "./resources/864630-chess/svg/008-pawn.svg"
+#define ROOK_IMG "./resources/864630-chess/svg/006-rook.svg"
+#define KNIGHT_IMG "./resources/864630-chess/svg/010-horse.svg"
+#define BISHOP_IMG "./resources/864630-chess/svg/016-bishop.svg"
+#define QUEEN_IMG "./resources/864630-chess/svg/007-queen.svg"
+#define KING_IMG "./resources/864630-chess/svg/009-king.svg"
 
 #endif
